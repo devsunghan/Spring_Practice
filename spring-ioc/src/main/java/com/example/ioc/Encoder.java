26cd830aecb946f8e5a26385b0a4fd16bb39ca06
임성한 <sunghanim0301@gmail.com>
2022-03-17 13:22:52 +0900

    v1 add spring-ioc

---
+++ spring-ioc/src/main/java/com/example/ioc/Encoder.java
@@ -0,0 +1,21 @@
+package com.example.ioc;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+public class Encoder {
+
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public void setiEncoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public String encode(String msg) {
+        return iEncoder.encode(msg);
+    }
+}
