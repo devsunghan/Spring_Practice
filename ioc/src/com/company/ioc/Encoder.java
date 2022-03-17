04791dac5e2fcdae7539784bccc92742cfe1d1f0
임성한 <sunghanim0301@gmail.com>
2022-03-17 13:21:09 +0900

    v1 add ioc

---
+++ ioc/src/com/company/ioc/Encoder.java
@@ -0,0 +1,14 @@
+package com.company.ioc;
+
+public class Encoder {
+
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public String encode(String msg) {
+        return iEncoder.encode(msg);
+    }
+}
