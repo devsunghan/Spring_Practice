6217ef242b3799824f55aace38a0572372df5bd8
임성한 <sunghanim0301@gmail.com>
2022-03-19 23:49:19 +0900

    v1 add validation

---
+++ validation/src/main/java/com/example/validation/dto/User.java
@@ -0,0 +1,99 @@
+package com.example.validation.dto;
+
+import com.example.validation.annotation.YearMonth;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class User {
+
+    @NotBlank
+    private String name;
+
+    @Max(value = 90)
+    private int age;
+
+    @Email
+    private String email;
+
+    @YearMonth
+    private String reqYearMonth; // yyyyMM
+
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. xxx-xxx(x)-xxxx")
+    private String phoneNumber;
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    private List<Car> cars;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
+    @AssertTrue(message = "yyyyMM 의 형식에 맞지 않습니다.")
+    public boolean isReqYearMonthValidation() {
+        try{
+            LocalDate localDate = LocalDate.parse(this.reqYearMonth, DateTimeFormatter.ofPattern("yyyyMM"));
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", cars=" + cars +
+                '}';
+    }
+}
